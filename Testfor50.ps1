1a50619daf5c761b62cdb7dfb08b5803d15f7f45
Joel Dunstan <jd427244@gmail.com>
2017-02-09 20:47:41 +0800

    1) Did testing for 70% and 60%, changing to 60% as per my recommendation. 2) Testing Documentation added.

--- Testfor50.ps1
+++ Testfor50.ps1
@@ -1,3 +1,3 @@
-for($i=0; $i -le 20; $i++) {
-	otree test basic_biosecurity_test 4 > "D:\OneDrive\I.T Jobs\UWA Ben White Biosecurity Project\Biosecurity Game\Instructions and Documentation\Testing\50% Tests\Test_$i.txt"
+for($i=1; $i -le 20; $i++) {
+	otree test basic_biosecurity_test 4 > "D:\OneDrive\I.T Jobs\UWA Ben White Biosecurity Project\Biosecurity Game\Instructions and Documentation\Testing\60% Tests\Test_$i.txt"
 }
