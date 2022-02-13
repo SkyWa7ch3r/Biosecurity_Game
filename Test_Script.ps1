c4b3de0aa7d65664877acb0caa9eb63204d20f60
SkyWa7ch3r <joel.g.w.dunstan@gmail.com>
2022-02-13 19:02:36 +0800

    Added these then I will clean up

---
+++ Test_Script.ps1
@@ -0,0 +1,12 @@
+for($i=1; $i -le 100; $i++) {
+	otree test basic_biosecurity_game 
+}
+for($i=1; $i -le 100; $i++) {
+	otree test low_risk_basic_biosecurity_game 
+}
+for($i=1; $i -le 100; $i++) {
+	otree test high_risk_basic_biosecurity_game 
+}
+for($i=1; $i -le 100; $i++) {
+	otree test very_high_risk_basic_biosecurity_game 
+}
