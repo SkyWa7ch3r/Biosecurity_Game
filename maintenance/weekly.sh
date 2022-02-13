a0d44e2f03629f33f7f64bb69039eb096cec7b67
SkyWa7ch3r <joel.g.w.dunstan@gmail.com>
2022-02-13 19:43:58 +0800

    Cleaning Up

--- maintenance/weekly.sh
+++
@@ -1,7 +0,0 @@
-pkill -9 daphne
-killall otree
-cd /usr/Biosecurity_Game_V2/
-/usr/pgsql-9.6/bin/pg_dump -a -U otree -h localhost django_db > /usr/postgres-backup/biosecurity-$(date +"%d-%m-%Y").sql
-echo y | otree resetdb
-psql -U otree django_db < /usr/postgres-backup/biosecurity-$(date +"%d-%m-%Y").sql
-sudo su -c "reboot"
